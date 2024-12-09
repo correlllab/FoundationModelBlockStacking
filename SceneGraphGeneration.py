770e8462d684fbba2022553cabd936470b01c2ad
corvus user <user@corvus@correlllab>
2024-12-09 11:01:22 -0700

    commit before moving scenegraph stuff

--- SceneGraphGeneration.py
+++ SceneGraphGeneration.py
@@ -78,8 +78,9 @@ class Node:
         
         self.rgb_segment = None
         self.depth_segment = None
-        self.pcd = None
-        self.pcd_bbox = None
+
+        self.points = None
+        self.colors = None
 
         self.calc_bbox(rgb_img, label_vit)
         self.calc_pcd(rgb_img, depth_img, K, depth_scale, observation_pose, sam_predictor)
@@ -118,36 +119,42 @@ class Node:
         transform_matrix = pose_vector_to_homog_coord(observation_pose)
         pcd.transform(transform_matrix)
         
-        
-        #if self.pcd is None:
-        self.pcd = pcd
-        #else:
-        #    self.pcd = self.pcd + pcd
+     
         #self.pcd, _ = self.pcd.remove_statistical_outlier(nb_neighbors=1000, std_ratio=1.0)
-        self.pcd, _ = self.pcd.remove_statistical_outlier(nb_neighbors=100, std_ratio=0.1)
-        self.pcd_bbox = self.pcd.get_axis_aligned_bounding_box()
+        pcd, _ = pcd.remove_statistical_outlier(nb_neighbors=100, std_ratio=0.1)
+        voxel_size = 0.005  # adjust based on your data
+        pcd = pcd.voxel_down_sample(voxel_size=voxel_size)
+        #self.pcd_bbox = self.pcd.get_axis_aligned_bounding_box()
         #self.pcd_bbox = pcd.get_minimal_oriented_bounding_box()
-        self.pcd_bbox.color = (1,0,0)
+        #self.pcd_bbox.color = (1,0,0)
+        self.points = np.asarray(pcd.points)
+        self.colors = np.asarray(pcd.colors)
         
-    def display(self, display_2d = True, display_3d = True):
-        if display_2d:
-            fig, axes = plt.subplots(ncols=2)
-            axes[0].imshow(self.rgb_segment)
-            axes[0].add_patch(get_observation_patch(self, "r"))
-            axes[0].text(self.pix_xmin, self.pix_ymin - 10, f"{self.str_label}", color='r', fontsize=12, ha='left', va='bottom')
-            axes[0].set_title("RGB segment")
+    def display(self):
+        fig, axes = plt.subplots(ncols=2, nrows=2)
+        axes[0, 0].imshow(self.rgb_segment)
+        axes[0, 0].add_patch(get_observation_patch(self, "r"))
+        axes[0, 0].text(self.pix_xmin, self.pix_ymin - 10, f"{self.str_label}", color='r', fontsize=12, ha='left', va='bottom')
+        axes[0, 0].set_title("RGB segment")
 
-            axes[1].imshow(self.depth_segment)
-            axes[1].add_patch(get_observation_patch(self, "r"))
-            axes[1].text(self.pix_xmin, self.pix_ymin - 10, f"{self.str_label}", color='r', fontsize=12, ha='left', va='bottom')
-            axes[1].set_title("Depth segment")
-        
-            plt.tight_layout()
-            #print(f"Showing observation for {self.str_label}")
-            plt.show(block = False)
-            plt.pause(1)  # Keeps the figure open for 3 seconds
-        if display_3d:
-            o3d.visualization.draw_geometries([self.pcd, self.pcd_bbox])
+        axes[0, 1].imshow(self.depth_segment)
+        axes[0, 1].add_patch(get_observation_patch(self, "r"))
+        axes[0, 1].text(self.pix_xmin, self.pix_ymin - 10, f"{self.str_label}", color='r', fontsize=12, ha='left', va='bottom')
+        axes[0, 1].set_title("Depth segment")
+    
+        plt.tight_layout()
+        #print(f"Showing observation for {self.str_label}")
+        plt.show(block = False)
+        plt.pause(1)  # Keeps the figure open for 3 seconds
+        ax = fig.add_subplot(223, projection='3d')
+        ax.scatter(self.points[:,0], self.points[:,1], self.points[:,2], c=self.colors, s=1)
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+        plt.title(f"{self.str_label} Point Cloud")
+        plt.show()
+ 
+            
 
 def get_graph(OAI_Client, label_vit, sam_predictor, rs_wrapper, UR_interface):
     G = nx.DiGraph()
@@ -173,17 +180,9 @@ def get_graph(OAI_Client, label_vit, sam_predictor, rs_wrapper, UR_interface):
         
     return G
 
-def get_geometries(G):
-    geometries = []
-    axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2, origin=[0, 0, 0])
-    geometries.append(axis)
-    if G is None:
-        return geometries
-
-    T = pose_vector_to_homog_coord(G.graph["observation_pose"])
-    axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.05, origin=[0, 0, 0])
-    axis.transform(T)
-    geometries.append(axis)
+def get_points(G):
+    points = []
+    colors = []
 
     for obj, node in G.nodes(data=True):
         #C = node["data"].pcd.get_center()
@@ -191,62 +190,58 @@ def get_geometries(G):
         #sphere.paint_uniform_color([0, 0, 0])  # Red color for sphere A
         #sphere.translate(C)
         #vis.add_geometry(sphere)
-        geometries.append(node["data"].pcd)
-        geometries.append(node["data"].pcd_bbox)
+        points.append(node["data"].points)
+        colors.append(node["data"].colors)
+    
+    points = np.concatenate(points, axis=0)
+    colors = np.concatenate(colors, axis=0)
+
+    return points, colors
+
+def display_graph(G, blocking = False):
+    
+    fig, axes = plt.subplots(ncols=2, nrows=1, figsize=(12, 12))
+    points, colors = get_points(G)
+    ax = fig.add_subplot(122, projection='3d')
+    ax.scatter(points[:,0], points[:,1], points[:,2], c=colors, s=0.5)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
 
     for edge in G.edges(data=True):
+        relation_str = f"{edge[0]} {edge[2]['connection']} {edge[1]}"
+
         source = edge[0]
         source_node = G.nodes[source]
-        source_centroid = source_node["data"].pcd.get_center()
+        source_centroid = np.mean(source_node["data"].points, axis=0)
 
         dest = edge[1]
         dest_node = G.nodes[dest]
-        dest_centroid = dest_node["data"].pcd.get_center()
+        dest_centroid = np.mean(dest_node["data"].points, axis=0)
 
         direction = dest_centroid - source_centroid
         length = np.linalg.norm(direction)
-        direction /= length  
 
-        arrow = o3d.geometry.TriangleMesh.create_arrow(
-            cylinder_radius=0.001, cone_radius=0.004, cone_height=0.004, cylinder_height=length
-        )
-        #arrow.translate([0, 0, -length / 2])  # Translate along z-axis to place base at origin
-        arrow.paint_uniform_color([0, 1, 0])  # RGB color (red)
-        rotation_axis = np.cross([0, 0, 1], direction)
-        angle = np.arccos(np.dot([0,0,1], direction))
-        R = o3d.geometry.get_rotation_matrix_from_axis_angle(rotation_axis*angle)
-        arrow.rotate(R, center=(0,0,0))  # Align the arrow to the direction
-        arrow.translate(source_centroid)
+        ax.quiver(source_centroid[0], source_centroid[1], source_centroid[2],
+                    direction[0], direction[1], direction[2],
+                    length=length, normalize=False, color='r')
+    
+    
 
-        geometries.append(arrow)
 
-        
-        relation_str = f"{edge[0]} {edge[2]['connection']} {edge[1]}"
-        mesh_text = o3d.t.geometry.TriangleMesh.create_text(relation_str, depth=12).to_legacy()
-        scaling_factor = 0.00025
-        mesh_text.scale(scaling_factor, center=[0,0,0])
 
-        mesh_text.paint_uniform_color([0, 0, 0])
 
-        text_pose = (source_centroid + dest_centroid)/ 2
-        mesh_text.translate(text_pose)
-
-        geometries.append(mesh_text)
-
-    return geometries
-
-def display_graph(G, display_2d = True, display_3d = True):
-    if display_2d:
-        fig, ax = plt.subplots()
-        pos = nx.spring_layout(G)
-        nx.draw(G, with_labels=True, node_color='lightblue', node_size=1500, font_size=15)
-        edge_labels = nx.get_edge_attributes(G, 'connection')
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=12)
-        plt.show(block = False)
+    pos = nx.spring_layout(G)
+    # Draw the graph on the specified axes
+    nx.draw(G, pos=pos, ax=axes[0], with_labels=True, node_color='lightblue', node_size=1500, font_size=15)
+    # Draw edge labels on the same axes
+    edge_labels = nx.get_edge_attributes(G, 'connection')
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels, font_size=12, ax=axes[0])
+    # Show without blocking, then pause
+    plt.tight_layout()
+    plt.show(block=blocking)
+    if not blocking:
         plt.pause(1)
-    if display_3d:
-        geometries = get_geometries(G)
-        o3d.visualization.draw_geometries(geometries)
 
 if __name__ == "__main__":
     from magpie_control import realsense_wrapper as real
@@ -285,11 +280,12 @@ if __name__ == "__main__":
     pose = homog_coord_to_pose_vector(myrobot.get_cam_pose())
     #__init__(self, str_label, rgb_img, depth_img, label_vit, sam_predictor, K, depth_scale, observation_pose)
 
-    obs = Node("dark blue block", rgb_img, depth_img, label_vit, sam_predictor, K, depth_scale, pose)
-    obs.display(display_2d = False, display_3d = False)
+    #obs = Node("dark blue block", rgb_img, depth_img, label_vit, sam_predictor, K, depth_scale, pose)
+    #obs.display()
 
     graph = get_graph(client, label_vit, sam_predictor, myrs, myrobot)
-    display_graph(graph, display_2d = True, display_3d = True)
+    display_graph(graph, blocking = True)
+
 
     myrobot.stop()
     myrs.disconnect()
